df2ab8e5248cb2a9f9f5e576128c9c1f2395ec60
Joe Cormier <joemcormier@outlook.com>
2020-09-29 17:54:09 -0400

    create project

--- projects/create_project.py
+++ projects/create_project.py
@@ -14,29 +14,28 @@ Directory Structure:
 """
 
 import os
-#current_dir = os.getcwd()
+# current_dir = os.getcwd()
 
 import sys
-#sys.path.append("/home/jmc/Documents/pycrash")
+
+# sys.path.append("/home/jmc/Documents/pycrash")
 sys.path.append("/home/joemcormier/pycrash/")
 
-#os.chdir("/home/joemcormier/pycrash/")
+# os.chdir("/home/joemcormier/pycrash/")
 print(os.getcwd())
 from pycrash.project import Project
 
-#os.chdir(current_dir)
+# os.chdir(current_dir)
 print(os.getcwd())
 
 project_name = 'validation sideswipe'
-project_inputs = {'name':project_name,
-                  'pdesc':'validate sideswipe models',
-				  'project_path':"/home/joemcormier/pycrash/projects/",
-                  'sim_type':'MV',
-                  'impact_type':'SS',
-                  'note':'validate sideswipe against published data'}
+project_inputs = {'name': project_name,
+                  'pdesc': 'validate sideswipe models',
+                  'project_path': "/home/joemcormier/pycrash/projects/",
+                  'sim_type': 'MV',
+                  'impact_type': 'SS',
+                  'note': 'validate sideswipe against published data'}
 
 proj = Project(project_inputs)
-
-
 
 # TODO: create script to ask for user inputs
